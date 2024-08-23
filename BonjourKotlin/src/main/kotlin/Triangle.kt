8d546d15a54e9d462304ed2bea0d21bf8ff65e17
David Nova <2327519@cegepmontpetit.ca>
2024-08-23 17:40:22 -0400

    FCT: exercise triangles finis

--- BonjourKotlin/src/main/kotlin/Triangle.kt
+++ BonjourKotlin/src/main/kotlin/Triangle.kt
@@ -1,2 +1,22 @@
 package org.novarincon
 
+fun main (args: Array<String>){
+    val hauteur:Int = args[0].toInt()
+
+    afficherTriangleh(hauteur)
+}
+fun afficherTriangleh(hauteur:Int){
+    val etoile:String="*"
+    for (i in 1..hauteur){
+        println(etoile.repeat(i))
+    }
+}
+
+//fun main(){
+//    val etoile:String="*"
+//   for (i in 1..4){
+//       println(etoile.repeat(i))
+//   }
+//}
+
+
